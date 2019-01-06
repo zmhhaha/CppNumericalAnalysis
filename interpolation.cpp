2012a91fb16504500f2207722e5098422ee5ebb3
zmhhaha <627955836@qq.com>
2019-01-06 20:06:42 +0800

    Update interpolation.cpp
    更新了插值程序

--- interpolation.cpp
+++ interpolation.cpp
@@ -4,17 +4,9 @@
 
 using namespace std;
 
-double linear_interpolation(double x_interpolation, double x1, double y1, double x2, double y2, double x3, double y3)
+double linear_interpolation(double x_interpolation, double x1, double y1, double x2, double y2)
 {
-	double L1;
-	if (x_interpolation<x2)
-	{
-		L1 = (x_interpolation - x1) / (x2 - x1)*y2 + (x2 - x_interpolation) / (x2 - x1)*y1;
-	}
-	else
-	{
-		L1 = (x_interpolation - x2) / (x3 - x2)*y3 + (x3 - x_interpolation) / (x3 - x2)*y2;
-	}
+	double L1 = (x_interpolation - x1) / (x2 - x1)*y2 + (x2 - x_interpolation) / (x2 - x1)*y1;
 	return L1;
 }
 
@@ -44,26 +36,63 @@ int main()
 	double x1, y1;
 	double x2, y2;
 	double x3, y3;
-	cout << "请输入作为插值参照的数据表：（x1，y1）、（x2，y2）、（x3，y3）"<<endl;
-	fout << "请输入作为插值参照的数据表：（x1，y1）、（x2，y2）、（x3，y3）" << endl;
 
-	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-	cout << "插值参数表：（" << x1 << "，" << y1 << "）、（" << x2 << "，" << y2 << "）、（" << x3 << "，" << y3 << "）"<<endl;
-	fout << "插值参数表：（" << x1 << "，" << y1 << "）、（" << x2 << "，" << y2 << "）、（" << x3 << "，" << y3 << "）" << endl;
+	int sw;
+	cout << "请选择需要插值的方法：" << endl;
+	cout << "1）线性插值" << endl;
+	cout << "2）二次插值" << endl;
+	cout << "请输入[1/2]" << endl;
+	cin >> sw;
+	cout << "你选择了";
+	fout << "你选择了";
 
+	switch (sw)
+	{
+	case 1:
+		cout << "线性插值" << endl;
+		fout << "线性插值" << endl;
+		cout << "请输入作为插值参照的数据表：（x1，y1）、（x2，y2）" << endl;
+		cout << "请输入数值并敲击回车，总共输入四次" << endl;
+		fout << "插值数据表：（x1，y1）、（x2，y2）" << endl;
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << "插值数据表：（" << x1 << "，" << y1 << "）、（" << x2 << "，" << y2 << "）" << endl;
+		fout << "插值数据表：（" << x1 << "，" << y1 << "）、（" << x2 << "，" << y2 << "）" << endl;
+		break;
+	case 2:
+		cout << "二次插值" << endl;
+		fout << "二次插值" << endl;
+		cout << "请输入作为插值参照的数据表：（x1，y1）、（x2，y2）、（x3，y3）" << endl;
+		cout << "请输入数值并敲击回车，总共输入六次" << endl;
+		fout << "插值数据表：（x1，y1）、（x2，y2）、（x3，y3）" << endl;
+		cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+		cout << "插值数据表：（" << x1 << "，" << y1 << "）、（" << x2 << "，" << y2 << "）、（" << x3 << "，" << y3 << "）" << endl;
+		fout << "插值数据表：（" << x1 << "，" << y1 << "）、（" << x2 << "，" << y2 << "）、（" << x3 << "，" << y3 << "）" << endl;
+		break;
+	default:
+		break;
+	}
+	
+	double L;
 	double x;
 	cout << "输入要插值的数值:";
 	cin >> x;
 	fout << "需要插值的数值:" << x << endl;
 
-	double L1 = linear_interpolation(x, x1, y1, x2, y2, x3, y3);
-
-	double L2 = parabolic_interpolation(x, x1, y1, x2, y2, x3, y3);
-
-	cout << "线性插值：" << L1 << endl;
-	fout << "线性插值：" << L1 << endl;
-	cout << "抛物插值：" << L2 << endl;
-	fout << "抛物插值：" << L2 << endl;
+	switch (sw)
+	{
+	case 1:
+		L = linear_interpolation(x, x1, y1, x2, y2);
+		cout << "线性插值：" << L << endl;
+		fout << "线性插值：" << L << endl;
+		break;
+	case 2:
+		L = parabolic_interpolation(x, x1, y1, x2, y2, x3, y3);
+		cout << "抛物插值：" << L << endl;
+		fout << "抛物插值：" << L << endl;
+		break;
+	default:
+		break;
+	}
 	
 	fout.close();
 	system("pause");
